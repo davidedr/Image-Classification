1f8abbf12ae953ef588bc1764d6a1857debe4347
Davide <d.dr@libero.it>
2017-09-05 22:30:18 +0200

    Random batch builder added

--- src/minibatch_builder.py
+++ src/minibatch_builder.py
@@ -6,11 +6,22 @@ Created on 05 set 2017
 
 import numpy as np
 
+# Dataset (or indexes into dataset array)
 idxs = np.arange(100)
 
+# Dataset is separated into batches of size batch_size
 batch_size = 10
 n_batches = len(idxs)//batch_size
 print('idxs :' + str(idxs) + '\nn_batches: ' + str(n_batches))
 
+for batch_i in range(n_batches):
+    print(idxs[batch_i * batch_size : (batch_i + 1)*batch_size])
+    
+# Now batches are randomly composed
+print()
+print('Batches of randomly chosen indexes')
+rand_idxs = np.random.permutation(idxs)
+
+idxs = rand_idxs
 for batch_i in range(n_batches):
     print(idxs[batch_i * batch_size : (batch_i + 1)*batch_size])
